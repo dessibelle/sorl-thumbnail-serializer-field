e4cffdf45f9b832798f0848186d23db18eac4851
Simon Fransson <simon@dessibelle.se>
2014-02-24 12:38:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 sorl-thumbnail-serializer-field
 ===============================
 
-A sorl-thumbnail field for user with django-rest-framework
+A sorl-thumbnail field for use with django-rest-framework
+
+Quick start
+-----------
+
+1. Add "polls" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = (
+        ...
+        'sorl_thumbnail_serializer',
+    )
+
+2. Add the `SorlThumbnailField` or `SorlHyperlinkedThumbnailField` to your serializer class.
+
+3. Specify the image dimensions when querying the REST API. Full size URL while be returned if dimensions are not specified.
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='sorl-thumbnail-serializer-field',
+    version='0.1',
+    packages=['sorl_thumbnail_serializer'],
+    include_package_data=True,
+    license='MIT License',
+    description='A sorl-thumbnail field for use with django-rest-framework.',
+    long_description=README,
+    url='https://github.com/dessibelle/sorl-thumbnail-serializer-field',
+    author='Simon Fransson',
+    author_email='simon@dessibelle.se',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'sorl-thumbnail',
+        'django-rest-framework',
+    ],
+)
