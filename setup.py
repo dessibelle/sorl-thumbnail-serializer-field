902cde0e77b6f5978f31063350e9eea0bb572606
Simon Fransson <simon@dessibelle.se>
2017-10-05 13:33:15 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sorl-thumbnail-serializer-field',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='MIT License',
